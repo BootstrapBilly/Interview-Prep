dc3cfa4b732021fa633cb25ec8ddb3c3c488baa8
Billy <billyjcatchpole@gmail.com>
2020-09-02 11:58:16 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './App.module.css';
+
+const App = () => {
+
+  return (
+
+    <div className={classes.container}>
+
+      app
+
+    </div>
+
+  );
+
+}
+
+export default App;
